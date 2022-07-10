2388b2f35e9694073f13a1c46542e58c8e57dce1
23_bott <59711850+23bottUA@users.noreply.github.com>
2022-07-10 18:57:39 -0500

    "Submit Works" -> "Submit"

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-/* Set the width of the sidebar to 250px and the left margin of the page content to 250px */
+/* Set the width of the sidebar to 250px and the left margin of the page content to 240px */
 function openNav() {
-  document.getElementById("mySidebar").style.width = "250px";
+  document.getElementById("mySidebar").style.width = "20%";
   document.getElementById("main").style.marginLeft = "240px";
 }
 
