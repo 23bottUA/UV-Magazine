2cf4bb090340ba59351c009d1d7dda035ee9498f
23_bott <59711850+23bottUA@users.noreply.github.com>
2022-07-10 19:20:54 -0500

    sidebar

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 /* Set the width of the sidebar to 250px and the left margin of the page content to 240px */
 function openNav() {
   document.getElementById("mySidebar").style.width = "20%";
-  document.getElementById("main").style.marginLeft = "240px";
+  document.getElementById("main").style.marginLeft = "19%";
 }
 
 /* Set the width of the sidebar to 0 and the left margin of the page content to 0 */
